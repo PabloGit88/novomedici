4c4edcfd9ba4868417153e512fdcd8b79291a880
Gecko <songecko@gmail.com>
2014-09-06 15:07:14 -0300

    videos

--- js/main.js
+++ js/main.js
@@ -3,14 +3,9 @@ $(function() {
     // Use Modernizr to detect for touch devices, 
     // which don't support autoplay and may have less bandwidth, 
     // so just give them the poster images instead
-    var screenIndex = 1,
-        numScreens = $('.screen').length,
-        isTransitioning = false,
-        transitionDur = 1000,
-        BV,
+    var BV,
         videoPlayer,
         isTouch = Modernizr.touch,
-        $bigImage = $('.big-image'),
         $window = $(window);
     
     if (!isTouch) {
@@ -18,68 +13,17 @@ $(function() {
         BV = new $.BigVideo({forceAutoplay:isTouch});
         BV.init();
         showVideo();
-        
-        BV.getPlayer().addEvent('loadeddata', function() {
-            onVideoLoaded();
-        });
-
-        // adjust image positioning so it lines up with video
-        $bigImage
-            .css('position','relative')
-            .imagesLoaded(adjustImagePositioning);
-        // and on window resize
-        $window.on('resize', adjustImagePositioning);
+    }
+    
+    function showVideo() 
+    {
+    	var videoBaseSrc = $('.videoBackground').data('videoBaseSrc');
+        BV.show([
+                 { type: "video/mp4",  src: videoBaseSrc + ".mp4" },
+                 { type: "video/webm", src: videoBaseSrc + ".webm" },
+                 { type: "video/ogg",  src: videoBaseSrc + ".ogv" }
+        ],
+        { ambient:true });
     }
 
-    function showVideo() {
-        BV.show($('#screen-'+screenIndex).attr('data-video'),{ambient:true});
-    }
-
-
-    function onVideoLoaded() {
-        $('#screen-'+screenIndex).find('.big-image').transit({'opacity':0},500)
-    }
-
-    function onTransitionComplete() {
-        isTransitioning = false;
-        if (!isTouch) {
-            $('#big-video-wrap').css('left',0);
-            showVideo();
-        }
-    }
-
-    function adjustImagePositioning() {
-        $bigImage.each(function(){
-            var $img = $(this),
-                img = new Image();
-
-            img.src = $img.attr('src');
-
-            var windowWidth = $window.width(),
-                windowHeight = $window.height(),
-                r_w = windowHeight / windowWidth,
-                i_w = img.width,
-                i_h = img.height,
-                r_i = i_h / i_w,
-                new_w, new_h, new_left, new_top;
-
-            if( r_w > r_i ) {
-                new_h   = windowHeight;
-                new_w   = windowHeight / r_i;
-            }
-            else {
-                new_h   = windowWidth * r_i;
-                new_w   = windowWidth;
-            }
-
-            $img.css({
-                width   : new_w,
-                height  : new_h,
-                left    : ( windowWidth - new_w ) / 2,
-                top     : ( windowHeight - new_h ) / 2
-            })
-
-        });
-
-    }
 });
